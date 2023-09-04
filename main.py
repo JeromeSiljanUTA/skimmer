b52c39c295bfc6f1912e33b0b166bf4b4b681ad8
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-09-04 11:14:22 -0500

    feat(repo): Reformatted with black, updated for 2023

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-if((input('Would you like to insert new entries? ').lower()) == 'yes' or 'y'):
+#!/home/jerome/misc/projects/programming/basic_venv/bin/python3
+if (input("Would you like to insert new entries? ").lower()) == "yes" or "y":
     import skimmer
+
     print("Inserting new entries...")
     skimmer.insert_new(skimmer.get_files())
     print("Done\n")
 else:
     print("No new entries inserted")
 
-if((input('Would you like to add new tags? ').lower()) == 'yes' or 'y'):
+if (input("Would you like to add new tags? ").lower()) == "yes" or "y":
     import skimmer
+
     skimmer.add_tags(skimmer.find_tagless())
 else:
     print("No new tags added")
 
-if((input('Would you like to generate reports? ').lower()) == 'yes' or 'y'):
+if (input("Would you like to generate reports? ").lower()) == "yes" or "y":
     import gen_report
+
     gen_report.generate()
     print("Success: Reports generated!")
 else:
--- skimmer.py
+++ skimmer.py
@@ -1,10 +1,12 @@
-# Set
+#!/home/jerome/misc/projects/programming/basic_venv/bin/python3
 # Imports and Datetime
 import pandas as pd
 import os
 import datetime
 import sqlite3
+
 date_time = datetime.datetime.now()
+
 
 # Grabbing Files
 def get_files():
@@ -32,21 +34,22 @@ def get_files():
 
     return paths
 
+
 def cleanup(customcash_path, altitude_path, cashplus_path, discover_path, wf_path):
     # CustomCash Cleanup
     customcash = pd.read_csv(customcash_path)
     customcash["Date"] = pd.to_datetime(customcash["Date"]).dt.date
     customcash = customcash[~(customcash["Description"].str.contains("THANK YOU"))]
-    customcash["Debit"].fillna(customcash.Credit, inplace = True)
+    customcash["Debit"].fillna(customcash.Credit, inplace=True)
     customcash["Category"] = "Gas Stations"
     customcash["Card"] = "Citi Custom Cash"
-    customcash = (customcash.drop(columns = ["Credit", "Status"])
-            .rename(columns = {"Description":"Name", "Debit":"Amount"})
-         )
+    customcash = customcash.drop(columns=["Credit", "Status"]).rename(
+        columns={"Description": "Name", "Debit": "Amount"}
+    )
     customcash = customcash[~(customcash["Name"].str.contains("AUTOPAY"))]
 
     # Altitude Cleanup
-    altitude = pd.read_csv(altitude_path, usecols = ["Amount", "Name", "Date"])
+    altitude = pd.read_csv(altitude_path, usecols=["Amount", "Name", "Date"])
     altitude["Date"] = pd.to_datetime(altitude["Date"]).dt.date
     altitude = altitude[~(altitude["Name"].str.contains("THANK YOU"))]
     altitude["Amount"] = altitude["Amount"] * -1
@@ -54,7 +57,7 @@ def cleanup(customcash_path, altitude_path, cashplus_path, discover_path, wf_pat
     altitude["Card"] = "USbank Altitude"
 
     # CashPlus Cleanup
-    cashplus = pd.read_csv(cashplus_path, usecols = ["Amount", "Name", "Date"])
+    cashplus = pd.read_csv(cashplus_path, usecols=["Amount", "Name", "Date"])
     cashplus["Date"] = pd.to_datetime(cashplus["Date"]).dt.date
     cashplus = cashplus[~(cashplus["Name"].str.contains("THANK YOU"))]
     cashplus["Amount"] = cashplus["Amount"] * -1
@@ -62,124 +65,147 @@ def cleanup(customcash_path, altitude_path, cashplus_path, discover_path, wf_pat
     cashplus["Card"] = "USbank CashPlus"
 
     # Discover Cleanup
-    discover = (pd.read_csv(discover_path, usecols = ["Trans. Date", "Amount", "Description", "Category"])
-                  .rename(columns = {"Trans. Date":"Date", "Description":"Name"})
-                )
+    discover = pd.read_csv(
+        discover_path, usecols=["Trans. Date", "Amount", "Description", "Category"]
+    ).rename(columns={"Trans. Date": "Date", "Description": "Name"})
     discover["Date"] = pd.to_datetime(discover["Date"]).dt.date
     discover = discover[~(discover["Name"].str.contains("THANK YOU"))]
     discover["Card"] = "Discover It"
 
     # Wells Fargo Cleanup
-    wf = (pd.read_csv(wf_path, header=None, usecols = [0, 1, 4])
-            .rename(columns = {0:"Date", 4:"Name", 1:"Amount"})
-         )
+    wf = pd.read_csv(wf_path, header=None, usecols=[0, 1, 4]).rename(
+        columns={0: "Date", 4: "Name", 1: "Amount"}
+    )
     wf["Amount"] = wf["Amount"] * -1
     wf["Date"] = pd.to_datetime(wf["Date"]).dt.date
     wf["Category"] = ""
     wf["Card"] = "Active Cash"
-    wf = wf[["Date", "Name", "Amount", "Category", "Card"] ]
+    wf = wf[["Date", "Name", "Amount", "Category", "Card"]]
 
-    # combine 
+    # combine
     comb = altitude.append(cashplus).append(discover).append(customcash).append(wf)
-    comb.sort_values("Date", inplace = True)
-    comb.reset_index(drop = True, inplace = True)
+    comb.sort_values("Date", inplace=True)
+    comb.reset_index(drop=True, inplace=True)
 
     return comb
 
+
 def find_tagless():
-    connection = sqlite3.connect('main.db')
+    connection = sqlite3.connect("main.db")
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM main WHERE ID NOT IN (SELECT ID FROM info)');
+    cursor.execute("SELECT * FROM main WHERE ID NOT IN (SELECT ID FROM info)")
     raw_tagless = cursor.fetchall()
 
     tagless = []
 
     for entry in raw_tagless:
-        tagless.append({"ID": str(entry[0]),
-                        "Date": str(entry[1]),
-                        "Name": str(entry[2]),
-                        "Amount": str(entry[3]),
-                        "Category": str(entry[4]),
-                        "Card": str(entry[5])})
+        tagless.append(
+            {
+                "ID": str(entry[0]),
+                "Date": str(entry[1]),
+                "Name": str(entry[2]),
+                "Amount": str(entry[3]),
+                "Category": str(entry[4]),
+                "Card": str(entry[5]),
+            }
+        )
 
     connection.close()
     return tagless
 
-def add_merchant(raw):
 
-# Merchant Dictionary
+def add_merchant(raw):
+    # Merchant Dictionary
     merchant_dict = {
-            "walmart":"Walmart",
-            "patel brothers":"Patel Brothers",
-            "amazon":"Amazon",
-            "jcpenney":"JCPenney",
-            "mcdonald":"McDonald's",
-            "buffalo wild wings":"Buffalo Wild Wings",
-            "family dollar":"Family Dollar",
-            "chick-fil-a":"Chick-fil-a",
-            "chickfil a":"Chick-fil-a",
-            "wendy's":"Wendy's",
-            "amzn":"Amazon",
-            "in n out":"In N Out",
-            "ihop":"IHOP",
-            "six flags":"Six Flags",
-            "panda":"Panda Express",
-            "waffle house":"Waffle House",
-            "starbucks":"Starbucks",
-            "walgreens":"Walgreens",
-            "wingstop":"Walgreens",
-            "braums":"Braum's",
-            "hershey's palace":"Hershey's Palace",
-            "gyros house":"Gyros House",
-            "shipley do-nuts":"Shipley Do-Nuts",
-            "ebay":"eBay",
-            "fuzzy taco":"Fuzzy Taco Shop",
-            "torchys":"Torchy's Tacos"
+        "walmart": "Walmart",
+        "patel brothers": "Patel Brothers",
+        "amazon": "Amazon",
+        "jcpenney": "JCPenney",
+        "mcdonald": "McDonald's",
+        "buffalo wild wings": "Buffalo Wild Wings",
+        "family dollar": "Family Dollar",
+        "chick-fil-a": "Chick-fil-a",
+        "chickfil a": "Chick-fil-a",
+        "wendy's": "Wendy's",
+        "amzn": "Amazon",
+        "in n out": "In N Out",
+        "ihop": "IHOP",
+        "six flags": "Six Flags",
+        "panda": "Panda Express",
+        "waffle house": "Waffle House",
+        "starbucks": "Starbucks",
+        "walgreens": "Walgreens",
+        "wingstop": "Walgreens",
+        "braums": "Braum's",
+        "hershey's palace": "Hershey's Palace",
+        "gyros house": "Gyros House",
+        "shipley do-nuts": "Shipley Do-Nuts",
+        "ebay": "eBay",
+        "fuzzy taco": "Fuzzy Taco Shop",
+        "torchys": "Torchy's Tacos",
     }
     raw_lower = raw.lower()
     for merchant in merchant_dict:
-        if(merchant in raw_lower):
+        if merchant in raw_lower:
             return merchant_dict[merchant]
 
     return raw
 
+
 def add_tags(tagless):
-    connection = sqlite3.connect('main.db')
+    connection = sqlite3.connect("main.db")
     cursor = connection.cursor()
 
     for entry in tagless:
-        prompt = ('Was the purchase on ' + entry['Date'] 
-                + ' for ' + entry['Amount'] 
-                + ' called ' + entry['Name'] 
-                + ' yours? ')
+        prompt = (
+            "Was the purchase on "
+            + entry["Date"]
+            + " for "
+            + entry["Amount"]
+            + " called "
+            + entry["Name"]
+            + " yours? "
+        )
         mine = (input(prompt)).lower()
 
-        if(mine == 'yes' or mine == 'y'):
-            cleaned = add_merchant(entry['Name'])
-            if(cleaned != entry['Name']):
+        if mine == "yes" or mine == "y":
+            cleaned = add_merchant(entry["Name"])
+            if cleaned != entry["Name"]:
                 merchant = cleaned
             else:
-                merchant = input('Enter merchant name: ')
+                merchant = input("Enter merchant name: ")
 
-            notes = input('Enter notes: ')
-            tags = input('Enter tags: ')
-            cursor.execute('INSERT INTO info(ID, Merchant, Notes, Tags) VALUES(' + entry["ID"] + ', "' + merchant + '", "' + notes + '", "' + tags + '")')
-        elif(mine == 'c'):
+            notes = input("Enter notes: ")
+            tags = input("Enter tags: ")
+            cursor.execute(
+                "INSERT INTO info(ID, Merchant, Notes, Tags) VALUES("
+                + entry["ID"]
+                + ', "'
+                + merchant
+                + '", "'
+                + notes
+                + '", "'
+                + tags
+                + '")'
+            )
+        elif mine == "c":
             connection.commit()
         else:
-            cursor.execute('INSERT INTO info(ID, Tags) VALUES("' + entry['ID'] + '", "Mom")')
-
+            cursor.execute(
+                'INSERT INTO info(ID, Tags) VALUES("' + entry["ID"] + '", "Mom")'
+            )
 
     connection.commit()
     connection.close()
 
+
 def insert_new(paths):
     main = cleanup(paths[3], paths[0], paths[1], paths[2], paths[4])
 
-    main.fillna("", inplace = True)
+    main.fillna("", inplace=True)
 
     # SQL Setup
-    connection = sqlite3.connect('main.db')
+    connection = sqlite3.connect("main.db")
     cursor = connection.cursor()
 
     all_rows = main.iterrows()
@@ -192,7 +218,15 @@ def insert_new(paths):
         category = '"' + str(row[3]) + '", '
         card = '"' + str(row[4]) + '"'
         try:
-            cursor.execute('INSERT INTO main(Date, Name, Amount, CAtegory, Card) VALUES (' + date + name + amount + category + card + ');')
+            cursor.execute(
+                "INSERT INTO main(Date, Name, Amount, Category, Card) VALUES ("
+                + date
+                + name
+                + amount
+                + category
+                + card
+                + ");"
+            )
         except sqlite3.IntegrityError:
             pass
 
