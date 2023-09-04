bc0944882a115b4b6d96d28b30c2c682d5666bfc
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-09-04 11:19:13 -0500

    fix(main.py): Fixed yes/no choices changed import statements

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 #!/home/jerome/misc/projects/programming/basic_venv/bin/python3
-if (input("Would you like to insert new entries? ").lower()) == "yes" or "y":
-    import skimmer
+from skimmer import insert_new, add_tags, get_files, find_tagless
+from gen_report import generate
 
+if (input("Would you like to insert new entries? ").lower()) == ("yes" or "y"):
     print("Inserting new entries...")
-    skimmer.insert_new(skimmer.get_files())
+    insert_new(get_files())
     print("Done\n")
 else:
     print("No new entries inserted")
 
-if (input("Would you like to add new tags? ").lower()) == "yes" or "y":
-    import skimmer
-
-    skimmer.add_tags(skimmer.find_tagless())
+if (input("Would you like to add new tags? ").lower()) == ("yes" or "y"):
+    add_tags(find_tagless())
 else:
     print("No new tags added")
 
-if (input("Would you like to generate reports? ").lower()) == "yes" or "y":
-    import gen_report
-
-    gen_report.generate()
+if (input("Would you like to generate reports? ").lower()) == ("yes" or "y"):
+    generate()
     print("Success: Reports generated!")
 else:
     print("No reports generated")
