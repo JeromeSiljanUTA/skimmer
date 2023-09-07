3764b9ce8f7a471d7e84039f19d0eb1b7ee48ffa
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2023-09-07 10:02:09 -0500

    feat(main.py): Consolidated input queries, added init_db()

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-#!/home/jerome/misc/projects/programming/basic_venv/bin/python3
-from skimmer import insert_new, add_tags, get_files, find_tagless
+from skimmer import insert_new, add_tags, get_files, find_tagless, init_db
 from gen_report import generate
 
-if (input("Would you like to insert new entries? ").lower()) == ("yes" or "y"):
-    print("Inserting new entries...")
+if (input("Would you like to insert new entries and add new tags? ").lower()) == (
+    "yes" or "y"
+):
+    init_db()
     insert_new(get_files())
-    print("Done\n")
-else:
-    print("No new entries inserted")
-
-if (input("Would you like to add new tags? ").lower()) == ("yes" or "y"):
     add_tags(find_tagless())
-else:
-    print("No new tags added")
 
 if (input("Would you like to generate reports? ").lower()) == ("yes" or "y"):
     generate()
